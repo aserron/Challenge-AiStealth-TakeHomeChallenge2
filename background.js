2df86039c691f67a7e14681aff087f874d6be33d
Andres Serron <aserron.dev@gmail.com>
2024-03-27 02:36:56 -0300

    works for: - tab switch - minimize maximize - focus. - switch win from same chrome profile.
    still not works:
    alt-tab.

--- background.js
+++ background.js
@@ -1,3 +1,27 @@
+/// <reference path="node_modules/@types/service_worker_api/index.d.ts" />
+
+/**
+ *
+ * @param {CallableFunction} fn
+ * @return {Promise<CallableFunction>}
+ */
+async function defer(fn) {
+    return new Promise((resolve) => {
+        setTimeout((cb) => {
+            return resolve(cb())
+        }, UI_DELAY_FIX, fn);
+    });
+}
+
+function isAllowedTitle(title) {
+    const allowed = [
+        TARGET_TITLE,
+        'chrome-extension://bdeihigdkcnekgbjggkcficjlcnfhjkn/index.html'
+    ]
+    return allowed.includes(title);
+}
+
+
 // background.js
 const UI_DELAY_FIX = 200;
 const TARGET_TITLE = "ai-stealth-challenge.tiiny.site"
@@ -7,56 +31,196 @@ const TARGET_HOST = "https://ai-stealth-challenge.tiiny.site/"
 let TAB_ID;
 let WIN_ID;
 
-/**
- * Activate Tab by update.
- * @param {number} tabId
- * @return {Promise<boolean>}
- */
-async function setActiveTabById(tabId) {
-    /**
-     *
-     * @type {chrome.tabs.UpdateProperties}
-     */
-    const updateProperties = {'active': true, autoDiscardable: false, highlighted: true};
+// track last processed win
+let previousWinId;
 
-    let result = false;
+// target original window minimized state.
+let isOriginalMinimized = false;
+
+// last target window options.
+let win_opt;
+
+// the background window we create with just a dummy about page.
+/**
+ * @type {chrome.windows.Window | undefined}
+ */
+let ABOUT;
+
+
+/**
+ *
+ * @param {number} tab
+ * @param window
+ */
+const sendTabBackgroundWin = async (tabId) => {
+
+    const tab = await chrome.tabs.get(tabId);
+    const prevWindowId = tab.windowId;
+
+    if (prevWindowId !== WIN_ID) return;
+
+
     try {
-        await chrome.tabs.update(tabId, updateProperties, (tab) => {
-            console.log("[setTabActive] update successful.  active=true");
-            result = true;
-        });
+        const prev = await chrome.windows.get(prevWindowId)
+        previousWinId = prev.id;
+
     } catch (e) {
-        console.error('[setTabActive] ', e);
+        console.error(e);
+        return;
     }
 
-    return Promise.resolve(result);
+    await chrome.tabs.create({windowId: prevWindowId, url: 'about:blank'})
+
+    await chrome.tabs.move(tabId, {windowId: ABOUT.id, index: 100000})
+    await chrome.tabs.update(tabId, {active: true})
+
+
+    await chrome.windows.update(prevWindowId, {state: 'minimized'});
+
 
 }
 
-// prevents minimization
-chrome.windows.onFocusChanged.addListener(function (windowId) {
+/**
+ *
+ * @param {number} winId
+ * @return {Promise<void>}
+ */
+const restoreTabOriginalWin = async (winId) => {
 
-    console.log("[windows.onFocusChanged] focus changed for id=%o", windowId);
+    const tab = await chrome.tabs.get(TAB_ID);
 
-    if (!WIN_ID) return;
+    const tabId = tab.id;
+    const prevWindowId = WIN_ID;
 
-    chrome.windows.getCurrent(async function (window) {
+    if (prevWindowId !== winId) return;
+    if (!tabId) return;
 
-        console.log('[windows.onFocusChanged] prevent target win from minimize and loose focus, id=%o', WIN_ID);
+    await chrome.tabs.move(tabId, {windowId: prevWindowId, index: 100000})
+    await chrome.tabs.update(tabId, {active: true})
 
-        await chrome.windows.update(WIN_ID, {
-            state: "normal",
-            drawAttention: true
+
+    await chrome.tabs
+        .query({url: 'about:blank', windowId: WIN_ID})
+        .then(tabs => tabs.map(tab => chrome.tabs.remove(tab.id)))
+
+    await chrome.windows.update(WIN_ID, {focused: true})
+
+}
+
+
+/**
+ *
+ * @param {number} tabId
+ * @param winId
+ */
+const moveTabIntoWin = async (tabId, winId = 0) => {
+
+    let tab = await chrome.tabs.get(tabId);
+
+    let newWindow = ABOUT;
+    let originalWinId = tab.windowId;
+
+    if (!tab) return;
+
+    return chrome.tabs
+        .move(tabId, {windowId: newWindow.id, index: 100000})
+        .then(movedTab => {
+            return chrome.tabs.update(movedTab.id, {active: true})
         })
-    });
-});
+}
 
-// prevent others tabs to deactivate the target window
-chrome.tabs.onActivated.addListener(function (/*TabActiveInfo*/ activeInfo) {
-    console.log("[chrome.tabs.onActivated]", activeInfo);
+async function setActiveWindowById(winId) {
+
+    console.info('[setWindowActive] id=%o', winId);
+
+    /**
+     * @type {chrome.windows.UpdateInfo}
+     */
+    const updateProperties = {
+        focused: false,
+        state: "normal",
+        drawAttention: true
+    };
+
+    let result = false;
+    try {
+        let tab = await chrome.windows.update(
+            winId,
+            updateProperties
+        );
+        result = true;
+    } catch (e) {
+        console.warn('[setWindowActive] ERROR id=%o', winId, e);
+
+    }
+
+    return result
+        ? Promise.resolve(result)
+        : Promise.reject(result);
+
+}
+
+async function createWithTabId(tabId = 0) {
+    return chrome.windows.create({
+        tabId: tabId,
+        focused: false,
+        state: "normal",
+        left: 500
+    })
+}
+
+async function removeAboutWin() {
+    if (ABOUT) {
+        try {
+            await chrome.windows.remove(ABOUT.id)
+        } catch (e) {
+            console.warn(`can not remove win`, ABOUT)
+        } finally {
+            ABOUT = undefined;
+        }
+    }
+}
+
+async function removeAboutTabs() {
+    return chrome.tabs
+        .query({url: 'about:blank'})
+        .then(tabs => tabs.map(tab => chrome.tabs.remove(tab.id)))
+        .then(results => {
+            console.info('closed about tabs', results)
+        })
+}
+
+async function createAboutWindow() {
+
+    await removeAboutTabs();
+
+
+    await removeAboutWin();
+
+    ABOUT = await chrome.windows.create({
+        url: "about:blank",
+        state: "normal",
+        focused: false,
+        top: 0,
+        left: 100,
+        width: 50,
+        height: 800
+    });
+
+    self.ABOUT = ABOUT;
+
+
+    return Promise.resolve(ABOUT);
+}
+
+
+/* LifeCycle */
+
+chrome.runtime.onInstalled.addListener(async (details) => {
+    console.info('[chrome.runtime.onInstalled] tab id=%o', details);
 })
 
-// just ensure the handlers are set in place.
+// main logic.
 chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
 
     const id = tabId;
@@ -66,17 +230,138 @@ chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
     if (changeInfo.status === 'complete') {
 
         // track the target demo window and tab ids.
-        if (tab.title === TARGET_TITLE) {
+        if (isAllowedTitle(tab.title)) {
+
+            try {
+                const win = await chrome.windows.get(tab.windowId);
+            } catch (e) {
+                console.error(e);
+                return;
+            }
+
+
             TAB_ID = tab.id;
             WIN_ID = tab.windowId;
+
+            self.TAB_ID = tab.id;
+            self.WIN_ID = tab.windowId;
+
+            self.TAB = tab;
+
+            ABOUT = await createAboutWindow();
+
+            console.log(ABOUT);
         }
 
-        chrome.tabs.onActivated.addListener(function (tab) {
-            console.info('[chrome.tabs.onActivated] tabId=%o', tab.tabId, tab);
-            // fix ui lag bug
-            setTimeout(async function () {
-                await setActiveTabById(TAB_ID);
-            }, UI_DELAY_FIX);
-        })
     }
 })
+
+
+// prevents minimization
+chrome.windows.onFocusChanged.addListener(async function (windowId) {
+
+    console.info("[windows.onFocusChanged] focus changed for windowId=%o and target=%o", windowId, WIN_ID);
+
+
+    if (WIN_ID === undefined) return;
+
+    /**
+     * @type {chrome.windows.Window}
+     */
+    const w = await chrome.windows.getCurrent();
+    const last = await chrome.windows.getLastFocused();
+
+
+    const isMinimized = windowId === chrome.windows.WINDOW_ID_NONE;
+    const isTarget = WIN_ID === w.id;
+
+
+    // restore tab to the original window when restoring window.
+    if (isOriginalMinimized && isTarget) {
+        return await restoreTabOriginalWin(WIN_ID)
+    }
+
+
+    if (isTarget && !win_opt) {
+        win_opt = {...w};
+        return
+    }
+
+    // update target window opt.
+    if (!isMinimized && isTarget && !!w) {
+        win_opt = {...w};
+        return;
+    }
+
+    if (!isMinimized) {
+        previousWinId = windowId;
+    }
+
+    if (!win_opt) {
+        console.warn('no win_opt', win_opt)
+        return;
+    }
+
+    const {alwaysOnTop, ...up} = win_opt;
+
+    const focused = win_opt.focused ?? false;
+
+    console.info(win_opt)
+
+
+    if (isTarget || last.id === previousWinId) {
+
+        isOriginalMinimized = true;
+
+        // return await moveTabIntoWin(TAB_ID)        
+
+        let res = false;
+        try {
+            await sendTabBackgroundWin(TAB_ID);
+            return res;
+        } catch (e) {
+            console.warn(`cant move tab id`, TAB_ID)
+        }
+    }
+
+    // minimized or changed focus: restore window
+    if (isTarget || last.id === previousWinId) {
+
+        console.info('update window:', win_opt)
+
+        let r1 = false;
+
+
+        try {
+            r1 = await setActiveWindowById(WIN_ID);
+        } catch (e) {
+            console.warn('[onFocusChanged] cant update', e);
+        }
+
+
+        let c = 0;
+        while (!r1 && !!chrome.runtime.lastError) {
+
+            console.log(`[onFocusChanged] loop ${c++} `, r1);
+
+            try {
+                await chrome.windows.update(WIN_ID, {
+                    ...up,
+                    focused: true
+                });
+                r1 = true;
+            } catch (e) {
+                console.warn('can not update window id:', WIN_ID)
+            }
+        }
+        return;
+
+    } else {
+        console.warn('this should not happen', windowId, w);
+    }
+
+
+});
+
+// shortcut.
+self.rw = restoreTabOriginalWin;
